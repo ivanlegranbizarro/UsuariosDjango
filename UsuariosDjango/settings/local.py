30e68fdd00e0a8558184b5d6bee193bfb82e7b0c
Iván <ivanlegranbizarro@gmail.com>
2021-02-28 16:28:03 +0100

    first commit

---
+++ UsuariosDjango/settings/local.py
@@ -0,0 +1,23 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'usuariosdjangodb',
+        'USER': 'postgres',
+        'PASSWORD': secret_settings.db_password,
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
